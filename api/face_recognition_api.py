f8c7baa12631456c673934684b948e3f9f8c2284
pierrechannel <pierrechannel0428@gmail.com>
2025-09-09 10:45:56 +0200

    rasbery pi threshold

--- api/face_recognition_api.py
+++ api/face_recognition_api.py
@@ -31,7 +31,7 @@ class FacialRecognitionAPI:
         
         # Ajustement du seuil pour Raspberry Pi
         if self.is_raspberry_pi:
-            self.THRESHOLD = 0.85  # Seuil plus élevé pour Raspberry Pi
+            self.THRESHOLD = 0.50  # Seuil plus élevé pour Raspberry Pi
             print(f"Raspberry Pi détecté - Seuil ajusté à {self.THRESHOLD}")
         else:
             self.THRESHOLD = 0.7
